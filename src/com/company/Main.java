e6eed7a5920770e9cc22ac69f70eb6d67ed99ef4
Mazhar-4321 <syedmazharali742@gmail.com>
2022-12-08 13:41:59 +0530

    ADDED: Methods to Calculate Permutation Of String

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
+    private static final int PERMUTATION_OF_STRINGS = 1;
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome To Algorithms");
+        System.out.println("Press 1 to Find Permutation Of Strings");
+        Main main = new Main();
+        int option = scanner.nextInt();
+        String inputString;
+        switch (option) {
+            case PERMUTATION_OF_STRINGS:
+                inputString = scanner.next();
+                main.findPermutationOfStrings(inputString);
+                break;
+        }
+    }
+
+    private void findPermutationOfStrings(String inputString) {
+
+    }
+    private void findPermutationOfStringsUsingRecursion(String inputString){
+
     }
 }
